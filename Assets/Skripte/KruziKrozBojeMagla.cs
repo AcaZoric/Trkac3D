e85e96e6234b73256e31db9929f490ee2ed927ff
Aleksandar Zoric <acazoric94@gmail.com>
2014-08-31 04:00:18 +0200

    Menjanje boja :)

---
+++ Assets/Skripte/KruziKrozBojeMagla.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KruziKrozBojeMagla : MonoBehaviour
+{
+    public float VremeKruzenja = 30f;
+    public float saturation = 1f;
+    public float brightness = 1f;
+
+	void Start ()
+    {
+        StartCoroutine("MenjajBoje");
+	}
+
+	IEnumerator MenjajBoje()
+    {
+        float t = 0f;
+        float hue = 1f;
+
+        while(t<=VremeKruzenja)
+        {
+            hue = Mathf.Lerp(1f, 0f, t / VremeKruzenja);
+            RenderSettings.fogColor = new HSBColor(hue, saturation, brightness).ToColor();
+            t += Time.deltaTime;
+            yield return 0;
+        }
+
+        hue = 0f;
+
+        StartCoroutine("MenjajBoje");
+    }
+}
---
+++ Assets/Skripte/KruziKrozBojeMaterijal.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KruziKrozBojeMaterijal : MonoBehaviour
+{
+    public float VremeKruzenja = 30f;
+    public float saturation = 1f;
+    public float brightness = 1f;
+
+	void Start ()
+    {
+        StartCoroutine("MenjajBoje");
+	}
+
+	IEnumerator MenjajBoje()
+    {
+        float t = 0f;
+        float hue = 0f;
+
+        while(t<=VremeKruzenja)
+        {
+            hue = Mathf.Lerp(0f, 1f, t / VremeKruzenja);
+            renderer.material.color = new HSBColor(hue, saturation, brightness).ToColor();
+            t += Time.deltaTime;
+            yield return 0;
+        }
+
+        hue = 1f;
+
+        StartCoroutine("MenjajBoje");
+    }
+}
