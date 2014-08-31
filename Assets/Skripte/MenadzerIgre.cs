d3540840117b000fa50f40263a7d32ae31880385
Aleksandar Zoric <acazoric94@gmail.com>
2014-08-31 02:15:37 +0200

    High Score uradjen

--- Assets/Skripte/MenadzerIgre.cs
+++ Assets/Skripte/MenadzerIgre.cs
@@ -8,17 +8,25 @@ public class MenadzerIgre : MonoBehaviour
     public int PoeniPoMetru = 1;
     public float Brzina = 20f;
     private float Score = 0f;
+    private static float HighScore = 0f;
     private bool krajIgre = false;
 	
     void Start()
     {
+        HighScore = PlayerPrefs.GetFloat("HighScore");
         menadzerIgre = this;
     }
+
     void Update()
     {
         if(!Igrac)
         {
             krajIgre = true;
+            if (Score > HighScore)
+            {
+                HighScore = Score;
+                PlayerPrefs.SetFloat("HighScore", HighScore);
+            }
         }
         else 
         {
@@ -34,6 +42,7 @@ public class MenadzerIgre : MonoBehaviour
     void OnGUI()
     {
         GUILayout.Label("Score: " + ((int)Score).ToString());
+        GUILayout.Label("High score: " + ((int)HighScore).ToString());
         if(krajIgre == true)
         {
             GUILayout.Label("Kraj igre! pritisni bilo koje dugme za restart");
